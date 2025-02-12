00944012192b5b3f4504139e0907e027b2f2fc23
GSERBAN <gabriel.serban@globallogic.com>
2025-02-12 10:38:47 +0200

    test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("s test");
+string myEnvVar = Environment.GetEnvironmentVariable("BRANCH_NAME");
+
+// Display it or use it as needed
+Console.WriteLine($"The environment variable BRANCH_NAME is: {myEnvVar}");
